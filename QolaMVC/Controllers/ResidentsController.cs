56ec29e6adf91fcb1811a0b93ef1142fd0be6319
Yuanchang Feng <fyc121574250@hotmail.com>
2018-09-07 23:55:07 -0400

    Save Button and Back Button

--- QolaMVC/Controllers/ResidentsController.cs
+++ QolaMVC/Controllers/ResidentsController.cs
@@ -3,6 +3,7 @@ using QolaMVC.Helpers;
 using QolaMVC.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -123,6 +124,101 @@ namespace QolaMVC.Controllers
             }
             return Json(l_Json, JsonRequestBehavior.AllowGet);
         }
+
+
+
+        [HttpPost]
+        public void saveButton_ApplicationSuite(DateTime term, int occu, int suitid, string notes)
+        {
+            var home = (HomeModel)TempData["Home"];
+            var user = (UserModel)TempData["User"];
+            var resident = (ResidentModel)TempData["Resident"];
+            TempData.Keep("User");
+            TempData.Keep("Home");
+            TempData.Keep("Resident");
+            update_Suite_Handler_Table.ApplicationSuite(home.Id,resident.ID,suitid, occu, term,notes,3,DateTime.Now);
+        }
+
+        [HttpPost]
+        public void saveButton_ChangeOccupancy(DateTime transferdate, int occu, string notes)
+        {
+            var home = (HomeModel)TempData["Home"];
+            var user = (UserModel)TempData["User"];
+            var resident = (ResidentModel)TempData["Resident"];
+            TempData.Keep("User");
+            TempData.Keep("Home");
+            TempData.Keep("Resident");
+            DateTime moveout = transferdate;
+            DateTime movein = transferdate.AddDays(1);
+            update_Suite_Handler_Table.ChangeOccupancy(home.Id, resident.ID, int.Parse(resident.SuiteNo), occu, movein,moveout, notes, 3, DateTime.Now);
+        }
+
+
+        [HttpPost]
+        public void saveButton_InternalTransfer(DateTime transferdate, int occu, int suitid, string notes)
+        {
+            var home = (HomeModel)TempData["Home"];
+            var user = (UserModel)TempData["User"];
+            var resident = (ResidentModel)TempData["Resident"];
+            TempData.Keep("User");
+            TempData.Keep("Home");
+            TempData.Keep("Resident");
+            DateTime moveout = transferdate;
+            DateTime movein = transferdate.AddDays(1);
+            update_Suite_Handler_Table.InternalTransfer(home.Id, resident.ID, suitid, occu, movein,moveout, notes, 3, DateTime.Now);
+        }
+
+        [HttpPost]
+        public void saveButton_TransfertoASCHOME(DateTime transferdate, int occu, int suitid, string notes, int homeid)
+        {
+            var home = (HomeModel)TempData["Home"];
+            var user = (UserModel)TempData["User"];
+            var resident = (ResidentModel)TempData["Resident"];
+            TempData.Keep("User");
+            TempData.Keep("Home");
+            TempData.Keep("Resident");
+            DateTime moveout = transferdate;
+            DateTime movein = transferdate.AddDays(1);
+            update_Suite_Handler_Table.TransfertoASCHOME(homeid, resident.ID, suitid, occu, movein, moveout, notes, 3, DateTime.Now);
+        }
+
+        [HttpPost]
+        public void saveButton_Normal_Move_Out(DateTime moveout, string notes, string reason)
+        {
+            var home = (HomeModel)TempData["Home"];
+            var user = (UserModel)TempData["User"];
+            var resident = (ResidentModel)TempData["Resident"];
+            TempData.Keep("User");
+            TempData.Keep("Home");
+            TempData.Keep("Resident");
+            update_Suite_Handler_Table.Normal_Move_Out(home.Id, resident.ID, int.Parse(resident.SuiteNo), resident.Occupancy, moveout, notes, DateTime.Now, reason);
+        }
+
+        [HttpPost]
+        public void saveButton_Passed_Away(DateTime moveout, string notes, DateTime passaway, string reason)
+        {
+            var home = (HomeModel)TempData["Home"];
+            var user = (UserModel)TempData["User"];
+            var resident = (ResidentModel)TempData["Resident"];
+            TempData.Keep("User");
+            TempData.Keep("Home");
+            TempData.Keep("Resident");
+            update_Suite_Handler_Table.Passed_away(home.Id, resident.ID, int.Parse(resident.SuiteNo), resident.Occupancy, moveout, notes, DateTime.Now, passaway, reason);
+        }
+
+        [HttpPost]
+        public void saveButton_Hospitalization(DateTime leaving, DateTime ExpectedReturn, DateTime ActualReturn, string notes, string reason)
+        {
+            var home = (HomeModel)TempData["Home"];
+            var user = (UserModel)TempData["User"];
+            var resident = (ResidentModel)TempData["Resident"];
+            TempData.Keep("User");
+            TempData.Keep("Home");
+            TempData.Keep("Resident");
+            update_Suite_Handler_Table.Hospitalization(home.Id, resident.ID, int.Parse(resident.SuiteNo), resident.Occupancy, leaving, ExpectedReturn, ActualReturn, notes, DateTime.Now, reason);
+        }
+
+
 
 
     }
---
+++ QolaMVC/DAL/update_Suite_Handler_Table.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace QolaMVC.DAL
+{
+    public class update_Suite_Handler_Table
+    {
+        public static void ApplicationSuite(int homeid,int redidentid,int suiteid,int occupancy,DateTime movein,string notes,int modify_by,DateTime modify_on)
+        {
+            using (var conn = new SqlConnection(Constants.ConnectionString.PROD))
+            using (var cmdGARead = new SqlCommand("Suite_Handler_ApplicationSuite", conn)
+            {
+                CommandType = CommandType.StoredProcedure
+            })
+            {
+                conn.Open();
+                cmdGARead.Parameters.AddWithValue("@homeID", homeid);
+                cmdGARead.Parameters.AddWithValue("@redidentid", redidentid);
+                cmdGARead.Parameters.AddWithValue("@suitid", suiteid);
+                cmdGARead.Parameters.AddWithValue("@occupancy", occupancy);
+                cmdGARead.Parameters.AddWithValue("@moveInDate", movein);
+                cmdGARead.Parameters.AddWithValue("@notes", notes);
+                cmdGARead.Parameters.AddWithValue("@modify_by", modify_by);
+                cmdGARead.Parameters.AddWithValue("@modify_on", modify_on);
+                cmdGARead.ExecuteNonQuery();
+            }
+        }
+
+        public static void ChangeOccupancy(int homeid, int redidentid, int suiteid, int occupancy, DateTime movein, DateTime moveout, string notes, int modify_by, DateTime modify_on)
+        {
+            using (var conn = new SqlConnection(Constants.ConnectionString.PROD))
+            using (var cmdGARead = new SqlCommand("Suite_Handler_ChangeOccupancy", conn)
+            {
+                CommandType = CommandType.StoredProcedure
+            })
+            {
+                conn.Open();
+                cmdGARead.Parameters.AddWithValue("@homeID", homeid);
+                cmdGARead.Parameters.AddWithValue("@redidentid", redidentid);
+                cmdGARead.Parameters.AddWithValue("@suitid", suiteid);
+                cmdGARead.Parameters.AddWithValue("@occupancy", occupancy);
+                cmdGARead.Parameters.AddWithValue("@moveInDate", movein);
+                cmdGARead.Parameters.AddWithValue("@moveoutdate", moveout);
+                cmdGARead.Parameters.AddWithValue("@notes", notes);
+                cmdGARead.Parameters.AddWithValue("@modify_by", modify_by);
+                cmdGARead.Parameters.AddWithValue("@modify_on", modify_on);
+                cmdGARead.ExecuteNonQuery();
+            }
+        }
+
+        public static void InternalTransfer(int homeid, int redidentid, int suiteid, int occupancy, DateTime movein, DateTime moveout, string notes, int modify_by, DateTime modify_on)
+        {
+            using (var conn = new SqlConnection(Constants.ConnectionString.PROD))
+            using (var cmdGARead = new SqlCommand("Suite_Handler_InternalTransfer", conn)
+            {
+                CommandType = CommandType.StoredProcedure
+            })
+            {
+                conn.Open();
+                cmdGARead.Parameters.AddWithValue("@homeID", homeid);
+                cmdGARead.Parameters.AddWithValue("@redidentid", redidentid);
+                cmdGARead.Parameters.AddWithValue("@suitid", suiteid);
+                cmdGARead.Parameters.AddWithValue("@occupancy", occupancy);
+                cmdGARead.Parameters.AddWithValue("@moveInDate", movein);
+                cmdGARead.Parameters.AddWithValue("@moveoutdate", moveout);
+                cmdGARead.Parameters.AddWithValue("@notes", notes);
+                cmdGARead.Parameters.AddWithValue("@modify_by", modify_by);
+                cmdGARead.Parameters.AddWithValue("@modify_on", modify_on);
+                cmdGARead.ExecuteNonQuery();
+            }
+        }
+
+        public static void TransfertoASCHOME(int homeid, int redidentid, int suiteid, int occupancy, DateTime movein, DateTime moveout, string notes, int modify_by, DateTime modify_on)
+        {
+            using (var conn = new SqlConnection(Constants.ConnectionString.PROD))
+            using (var cmdGARead = new SqlCommand("Suite_Handler_TransfertoASCHOME", conn)
+            {
+                CommandType = CommandType.StoredProcedure
+            })
+            {
+                conn.Open();
+                cmdGARead.Parameters.AddWithValue("@homeID", homeid);
+                cmdGARead.Parameters.AddWithValue("@redidentid", redidentid);
+                cmdGARead.Parameters.AddWithValue("@suitid", suiteid);
+                cmdGARead.Parameters.AddWithValue("@occupancy", occupancy);
+                cmdGARead.Parameters.AddWithValue("@moveInDate", movein);
+                cmdGARead.Parameters.AddWithValue("@moveoutdate", moveout);
+                cmdGARead.Parameters.AddWithValue("@notes", notes);
+                cmdGARead.Parameters.AddWithValue("@modify_by", modify_by);
+                cmdGARead.Parameters.AddWithValue("@modify_on", modify_on);
+                cmdGARead.ExecuteNonQuery();
+            }
+        }
+
+        public static void Normal_Move_Out(int homeid, int redidentid, int suiteid, int occupancy, DateTime moveout, string notes, DateTime modify_on, string reason)
+        {
+            using (var conn = new SqlConnection(Constants.ConnectionString.PROD))
+            using (var cmdGARead = new SqlCommand("Suite_Handler_Normal_Move_Out", conn)
+            {
+                CommandType = CommandType.StoredProcedure
+            })
+            {
+                conn.Open();
+                cmdGARead.Parameters.AddWithValue("@homeID", homeid);
+                cmdGARead.Parameters.AddWithValue("@redidentid", redidentid);
+                cmdGARead.Parameters.AddWithValue("@suitid", suiteid);
+                cmdGARead.Parameters.AddWithValue("@occupancy", occupancy);
+                cmdGARead.Parameters.AddWithValue("@moveoutdate", moveout);
+                cmdGARead.Parameters.AddWithValue("@notes", notes);
+                cmdGARead.Parameters.AddWithValue("@modify_on", modify_on);
+                cmdGARead.Parameters.AddWithValue("@reason", reason);
+                cmdGARead.ExecuteNonQuery();
+            }
+        }
+
+        public static void Passed_away(int homeid, int redidentid, int suiteid, int occupancy, DateTime moveout, string notes, DateTime modify_on, DateTime passedaway, string reason)
+        {
+            using (var conn = new SqlConnection(Constants.ConnectionString.PROD))
+            using (var cmdGARead = new SqlCommand("Suite_Handler_Passed_away", conn)
+            {
+                CommandType = CommandType.StoredProcedure
+            })
+            {
+                conn.Open();
+                cmdGARead.Parameters.AddWithValue("@homeID", homeid);
+                cmdGARead.Parameters.AddWithValue("@redidentid", redidentid);
+                cmdGARead.Parameters.AddWithValue("@suitid", suiteid);
+                cmdGARead.Parameters.AddWithValue("@occupancy", occupancy);
+                cmdGARead.Parameters.AddWithValue("@moveoutdate", moveout);
+                cmdGARead.Parameters.AddWithValue("@notes", notes);
+                cmdGARead.Parameters.AddWithValue("@modify_on", modify_on);
+                cmdGARead.Parameters.AddWithValue("@pass_away_date", passedaway);
+                cmdGARead.Parameters.AddWithValue("@reason", reason);
+                cmdGARead.ExecuteNonQuery();
+            }
+        }
+
+        public static void Hospitalization(int homeid, int redidentid, int suiteid, int occupancy, DateTime leaving, DateTime ExpectedReturn, DateTime ActualReturn, string notes, DateTime modify_on, string reason)
+        {
+            using (var conn = new SqlConnection(Constants.ConnectionString.PROD))
+            using (var cmdGARead = new SqlCommand("Suite_Handler_Hospitalization", conn)
+            {
+                CommandType = CommandType.StoredProcedure
+            })
+            {
+                conn.Open();
+                cmdGARead.Parameters.AddWithValue("@homeID", homeid);
+                cmdGARead.Parameters.AddWithValue("@redidentid", redidentid);
+                cmdGARead.Parameters.AddWithValue("@suitid", suiteid);
+                cmdGARead.Parameters.AddWithValue("@occupancy", occupancy);
+                cmdGARead.Parameters.AddWithValue("@leaving", leaving);
+                cmdGARead.Parameters.AddWithValue("@expectedreturn", ExpectedReturn);
+                cmdGARead.Parameters.AddWithValue("@actualreturn", ActualReturn);
+                cmdGARead.Parameters.AddWithValue("@notes", notes);
+                cmdGARead.Parameters.AddWithValue("@modify_on", modify_on);
+                cmdGARead.Parameters.AddWithValue("@reason", reason);
+                cmdGARead.ExecuteNonQuery();
+            }
+        }
+
+
+
+
+    }
+}
