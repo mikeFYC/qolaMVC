0585faec5fb41725de25ec6a1bd031c7ed0efbf3
Yuanchang Feng <fyc121574250@hotmail.com>
2019-03-15 10:09:42 -0400

    aaa_test

--- QolaMVC/EF_DAL/QolaModel.Designer.cs
+++ QolaMVC/EF_DAL/QolaModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\okwuo\Source\Repos\QolaMVC_Final\QolaMVC\EF_DAL\QolaModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Repos\qolaMVC\QolaMVC\EF_DAL\QolaModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
--- QolaMVC/Model1.Designer.cs
+++ QolaMVC/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Roses\Dropbox\NEW_QOLA\QolaMVC_Final\QolaMVC\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Repos\qolaMVC\QolaMVC\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
